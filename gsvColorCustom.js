2083f45ae42738425b87eb8d920845bc9f44d1dc
SHIN Yongsoo <yohns@kakao.com>
2024-08-23 20:25:54 +0900

    색상 전부 추가함

--- gsvColorCustom.js
+++ gsvColorCustom.js
@@ -5,13 +5,43 @@ document.querySelectorAll('.sc-brqgnP').forEach((element) => {
         const lastElement = eQxToMElements[eQxToMElements.length - 1];
         const value = parseFloat(lastElement.textContent);
 
-        if (value >= 170) {
+        if (value >= 170) {//무지개
             lastElement.style.background = "linear-gradient(to left ,#1E9600,#FFF200,#FF0000)";
-
-        }else if(value < 170 && value >= 160){
+        }else if(value < 170 && value >= 160){//금
             lastElement.style.background = "linear-gradient(to right,#ffe259,#ffa751)";
-        }else if(value<160){
+        }else if(value<160  && value >= 150){//은
             lastElement.style.background = " linear-gradient(to top, #ada996, #f2f2f2, #dbdbdb, #eaeaea)";
+        }else if(value < 150  && value >= 140)   {//동
+            lastElement.style.background = " linear-gradient(#DD8844, #DD8844)";
+        }else if(value < 140  && value >= 130)   {//빨그라
+            lastElement.style.background = " linear-gradient(#FF0000, #FFFFFF)";
+        }else if(value < 130  && value >= 120)   {//빨
+            lastElement.style.background = " linear-gradient(#FF0000, #FF0000)";
+        }else if(value < 120  && value >= 110)   {//보그라
+            lastElement.style.background = " linear-gradient(#FF00FF, #FFFFFF)";
+        }else if(value < 110  && value >= 100)   {//보
+            lastElement.style.background = " linear-gradient(#FF00FF, #FF00FF)";
+        } else if(value < 100  && value >= 90)   {//파그라
+            lastElement.style.background = " linear-gradient(#3366FF, #FFFFFF)";
+        } else if(value < 90  && value >= 80)   {//파
+            lastElement.style.background = " linear-gradient(#3366FF, #3366FF)";
+        } else if(value < 80  && value >= 70)   {//초그라
+            lastElement.style.background = " linear-gradient(#33FF00, #FFFFFF)";
+        } else if(value < 70  && value >= 60)   {//초
+            lastElement.style.background = " linear-gradient(#33FF00, #33FF00)";
+        } else if(value < 60  && value >= 50)   {//노그라
+            lastElement.style.background = " linear-gradient(#FFFF00, #FFFFFF)";
+        } else if(value < 50  && value >= 40)   {//노
+            lastElement.style.background = " linear-gradient(#FFFF00, #FFFF00)";
+        } else if(value < 40  && value >= 30)   {//주그라
+            lastElement.style.background = " linear-gradient(#FF6600, #FFFFFF)";
+        } else if(value < 30  && value >= 20)   {//주
+            lastElement.style.background = " linear-gradient(#FF6600, #FF6600)";
+        } else{
+            lastElement.style.background = " linear-gradient(#FFFFFF, #FFFFFF)";
+            lastElement.style.color = "black";
         }
+
+
     }
 });
